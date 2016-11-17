c15248bd0bb1e7e75950950f13d33dbca22feb32
Noah-I <noah@mavenly.com>
2016-11-17 11:49:03 -0500

    init commit

---
+++ Documents/classs/class_apps/overdue/db/migrate/20161117161038_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.string :author
+      t.datetime :due_date
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
