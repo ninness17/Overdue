cabf50b7d4c8b8cce7768624a2d30e970a4b0e77
Noah-I <noah@mavenly.com>
2016-11-17 20:53:34 -0500

    finished books and user model

--- Documents/classs/class_apps/overdue/config/routes.rb
+++ Documents/classs/class_apps/overdue/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   resources :books
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'books#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
